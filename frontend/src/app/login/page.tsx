d139a8009c4cca5dfc68beec90dbfc230a24527e
unknown <lokesh2006172@gmail.com>
2025-09-12 01:07:04 +0530

    Corrected a npm run build error:

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,10 +1,20 @@
+import { Suspense } from "react";
 import LoginPage from "@/components/auth-pages/login";
 import Navbar from "@/components/home-page/components/navbar";
+
 const Page = () => {
   return (
     <div className="min-h-screen font-inter bg-white">
       <Navbar />
-      <LoginPage />
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center min-h-screen">
+            Loading...
+          </div>
+        }
+      >
+        <LoginPage />
+      </Suspense>
     </div>
   );
 };
